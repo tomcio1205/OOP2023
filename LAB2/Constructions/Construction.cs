1f1cf89b81b4fe571972219faff0427610674f1b
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2023-10-19 15:49:25 +0200

    lab2

---
+++ LAB2/Constructions/Construction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2.Constructions
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entrances, 
+            int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
---
+++ LAB2/Program.cs
@@ -0,0 +1,14 @@
+﻿using LAB2.Constructions;
+using System;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(23, 45, 4, 6, "Brik");
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
